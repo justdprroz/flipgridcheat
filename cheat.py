091a8c4465e63d64f9da394378282301e81cabef
justdprroz <dmitri.proskuriakov@gmail.com>
2021-03-09 19:34:17 +0300

    Added config for link

--- cheat.py
+++ cheat.py
@@ -2,12 +2,14 @@ import platform
 
 from selenium import webdriver
 
+from videourl import url
+
 if platform.system() == "Linux":
     driver = webdriver.Chrome("./chromedriver")
 if platform.system() == "Windows":
     driver = webdriver.Chrome("./chromedriver.exe")
 
-driver.get("https://flipgrid.com/12b8fe2f")
+driver.get(url)
 while (True):
     driver.find_element_by_class_name("likeButton").click()
     driver.refresh()
